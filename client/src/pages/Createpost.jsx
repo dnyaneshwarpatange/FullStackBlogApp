20c460c09e7ef9acd1e46965de0ff004ec810fe1
Dnyaneshwar Patange <patangednyaneshwar7@gmail.com>
2024-08-18 11:59:04 +0530

    improved create post page

--- client/src/pages/Createpost.jsx
+++ client/src/pages/Createpost.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 
 const Createpost = () => {
   return (
-    <div className="sigup flex flex-col w-full m-auto my-32">
-      <div className="inputcontainer flex flex-col gap-2">
-        <label className='text-white text-center' htmlFor="username">Add Image</label>
-        <input className=' px-2 py-1 mx-2 my-2 rounded-xl bg-white text-center align-middle justify-center' type="file" />
-        <label className='text-white text-center' htmlFor="email">Title</label>
-        <input className=' px-2 py-1 mx-2 my-2 h-20 rounded-xl bg-white' type="text" />
-        <label className='text-white text-center' htmlFor="password">Description</label>
-        <input className=' px-2 py-1 mx-2 my-2 rounded-xl h-80 w-90 bg-white' type="text" />
-      </div>
-      <div className="submitbutton my-5 rounded-xl">
-        <button className='rounded-xl mx-2 my-2 px-2 py-1 bg-orange-400 text-white'>Create Post</button>
-      </div>
+    <div className="signup bg-[rgb(99,106,109)] h-[35rem] w-[50rem] flex gap-2 m-auto my-12 rounded-xl flex-col justify-center">
+      <label className='mx-[20rem] text-center h-6 w-[10rem] rounded-md text-white px-2 bg-gray-950' htmlFor="imageurl">Enter URL of Image</label>
+      <input className='mx-[8rem] bg-white h-6 w-[30rem] rounded-md' type="text" />
+
+      <label className='mx-[20rem] h-6 w-[11rem] rounded-md text-white px-2 bg-gray-950' htmlFor="title">Enter Title-min 5 char </label>
+      <input className='mx-[8rem] bg-white h-6 w-[30rem] rounded-md' type="text" />
+
+
+      <label className='mx-[18rem] h-6 w-[15rem] rounded-md text-white px-2 bg-gray-950' htmlFor="imageurl">Enter Description-min 50 char</label>
+      <input className='mx-[5rem] bg-white h-[20rem] w-[40rem] rounded-md' type="text" />
+
+      <button className='bg-[rgb(57,71,77)] w-[4rem] rounded-xl mx-20 text-white text-lg border-spacing'>Post</button>
+
     </div>
     
   )
