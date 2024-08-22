fc2502a3bad05985d50915f19f99b3c5f7def897
Dnyaneshwar Patange <patangednyaneshwar7@gmail.com>
2024-08-22 20:47:35 +0530

    vercel config

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 app.use(express.json());
 
 const corsOptions = {
-    origin: ["http://localhost:5173","https://mblog-pearl.vercel.app"],
+    origin: ["http://localhost:5173","https://blogx-dusky.vercel.app"],
     method: ["GET","POST","PUT","DELETE"],
     Credential:true,
 };
